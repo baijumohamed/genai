8e9a406369b9f3b56e5c4d87bc484d3bf559f222
baiju <baiju@trivand.com>
2024-05-30 13:24:00 +0100

    GENAI-3 - Set up Git Integration

---
+++ src/test/java/com/baiju/genai/GenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baiju.genai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
